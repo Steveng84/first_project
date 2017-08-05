de00a950c9837a9c1da6962356e504089b0f34dd
steveng84 <stevengordonpedersen@gmail.com>
2017-08-05 21:23:13 +0200

    Latest changes

--- app/controllers/say_controller.rb
+++ app/controllers/say_controller.rb
@@ -5,5 +5,14 @@ class SayController < ApplicationController
 
   def goodbye
     @files = Dir.glob('*')
+
   end
+  def say_goodnight(name)
+    "Good Night, #{name.capitalize}"
+    
+  end
+  puts say_goodnight('pa')
+end
+if line =~ /P(erl|ython)/
+  puts "There seems to be another scripting language here"
 end
--- db/migrate/20160828115944_create_tweets.rb
+++ db/migrate/20160828115944_create_tweets.rb
@@ -3,6 +3,10 @@ class CreateTweets < ActiveRecord::Migration
     create_table :tweets do |t|
       t.string :name
       t.text :content
+      t.string :image_url
+      t.decimal :price, precision: 8, scale: 2
+
+      t.timestamps
     end
   end
 end
